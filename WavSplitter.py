4dee85cbb7db8159d7b4a359a955ceaff3569f50
KaMZeSs <vladd267@gmail.com>
2023-08-08 13:51:42 +0300

    Added last segment exception if less than length

--- WavSplitter.py
+++ WavSplitter.py
@@ -7,7 +7,6 @@ import sys
 def process_folder(input_folder, output_folder, segment_length):
 
     try:
-        # Создаем выходную папку, если она не существует
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
     except:
@@ -15,27 +14,31 @@ def process_folder(input_folder, output_folder, segment_length):
         print(traceback.format_exc())
 
 
-    # Получаем список всех WAV файлов в исходной папке
+    # Get all wav files from input folder
     wav_files = [f for f in os.listdir(input_folder) if f.endswith('.wav')]
 
     for wav_file in wav_files:
         print(f'Processing: {wav_file}')
 
         try:
-            # Загружаем аудиофайл
+            # Load audio file
             audio = AudioSegment.from_wav(os.path.join(input_folder, wav_file))
         except:
             print(f'Error: loading wav file - {wav_file}')
             print(traceback.format_exc())
         
         try:
-            # Разбиваем аудиофайл на сегменты
+            # Splitting audio file
             segments = [audio[i:i+segment_length] for i in range(0, len(audio), segment_length)]
         except:
             print(f'Error: segmenting wav file - {wav_file}')
             print(traceback.format_exc())
+
+        # Exclude the last segment if its length does not meet the requirements
+        if len(segments[-1]) < segment_length:
+            segments = segments[:-1]
         
-        # Сохраняем сегменты в выходной папке
+        # Saving segments
         for i, segment in enumerate(segments):
             output_file = os.path.join(output_folder, f'{os.path.splitext(wav_file)[0]}_{i+1}.wav')
             try:
@@ -49,7 +52,7 @@ def process_folder(input_folder, output_folder, segment_length):
 if __name__ == "__main__":
     arguments = sys.argv[1:]  # Получаем все аргументы командной строки, кроме имени скрипта
 
-    # Здесь вы можете обрабатывать аргументы как вам необходимо
+    # Обрабатываем аргументы
     input_folder = str('.\input')
     output_folder = str('.\out')
     segment_length = int(3000)
